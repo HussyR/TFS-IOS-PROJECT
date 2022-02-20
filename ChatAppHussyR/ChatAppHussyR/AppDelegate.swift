c2c67a02adba690079ad1472034627c34ed9b68e
Danila <danil.ryabikov@bk.ru>
2022-02-20 16:02:00 +0300

    5 task

--- ChatAppHussyR/ChatAppHussyR/AppDelegate.swift
+++ ChatAppHussyR/ChatAppHussyR/AppDelegate.swift
@@ -9,31 +9,38 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+    // предполагаю что 5 задание решается так
+    var logging = true
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        guard logging else {return true}
         print("Application moved from \"Not running\" to \"Inactive\":\(#function)")
         return true
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
+        guard logging else {return}
         print("Application moved from \"Inactive\" to \"Active\":\(#function)")
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+        guard logging else {return}
         print("Application moved from \"Active\" to \"Inaclive\":\(#function)")
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
+        guard logging else {return}
         print("Application moved from \"Inactive\" to \"Background\":\(#function)")
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
+        guard logging else {return}
         print("Application moved from \"Background\" to \"Inactive\":\(#function)")
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
+        guard logging else {return}
         print("Application moved from \"Background\" to \"Not Running\":\(#function)")
     }
     
--- ChatAppHussyR/ChatAppHussyR/ViewController.swift
+++ ChatAppHussyR/ChatAppHussyR/ViewController.swift
@@ -8,43 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var logging = (UIApplication.shared.delegate as? AppDelegate)?.logging
 }
 
 //MARK: Lifecycle
 extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let logging = logging,
+              logging
+        else {return}
         print(#function)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        guard let logging = logging,
+              logging
+        else {return}
         print(#function)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        guard let logging = logging,
+              logging
+        else {return}
         print(#function)
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
+        guard let logging = logging,
+              logging
+        else {return}
         print(#function)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        guard let logging = logging,
+              logging
+        else {return}
         print(#function)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        guard let logging = logging,
+              logging
+        else {return}
         print(#function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        guard let logging = logging,
+              logging
+        else {return}
         print(#function)
     }
 
